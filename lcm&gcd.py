95f6c1169c2dd7e2b90ea963ecdab135d86ce19c
Harish <harishkumarsp023@gmail.com>
2024-05-21 20:17:58 +0530

    lcm &gcd

--- lcm&gcd.py
+++ lcm&gcd.py
@@ -1,36 +1,13 @@
-                                
-def findGcd(n1, n2):
-    # Iterate from the minimum of
-    # n1 and n2 down to 1
-    # Start from the minimum of n1 and n2
-    # because the GCD cannot
-    # exceed the smaller number
-    
-    for i in range(min(n1, n2), 0, -1):
-        # Check if i is a common
-        # factor of both n1 and n2
-        if n1 % i == 0 and n2 % i == 0:
-            # If i is a common factor,
-            # return it as the GCD
-            return i
-    # If no common factors are found,
-    # return 1 (as 1 is always a
-    # divisor of any number)
-    return 1
+#User function Template for python3
 
-
-def main():
-    n1 = 20
-    n2 = 15
-    
-    # Find the GCD of n1 and n2
-    gcd = findGcd(n1, n2)
-
-    print("GCD of", n1, "and", n2, "is:", gcd)
-
-
-if __name__ == "__main__":
-    main()
-    
-                                
-                            
+class Solution:
+    def lcmAndGcd(self, A , B):
+        # code here 
+        a=[0,0] 
+        for i in range(1,min(A,B)+1):
+             if A % i == 0 and B % i ==0:
+                
+                 a[1] =i
+        l=(A*B)// a[1]
+        a[0]=l
+        return a
