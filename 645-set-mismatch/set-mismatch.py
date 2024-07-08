a06f9efcd4b8951556e70f5d8769873ab6964197
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-07-08 09:27:08 +0530

    Time: 162 ms (51.86%) | Memory: 18.3 MB (43.08%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -4,7 +4,6 @@ class Solution:
         ans=0
         for i in nums:
             my[i]=my.get(i,0)+1
-        print(my)
         m=0
         for j in range(1,len(nums)+1):
             if j in my:    
