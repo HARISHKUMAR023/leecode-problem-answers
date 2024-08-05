d287da1a2eefd097481a8254548f1a439abcd629
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-08-05 08:22:14 +0530

    Time: 233 ms (40.49%) | Memory: 19.4 MB (83.45%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-      
-       my = defaultdict(int)
-       my[0] = 1  
-       presum = 0
-       cut = 0
-       for i in range(len(nums)):
-            presum += nums[i]
-            r = presum - k
-            cut += my[r]
-            my[presum] += 1
+     my={0:1}
+     presum =0 
+     c=0
+     for num in nums:
+        presum+=num
+        r=presum-k
+        if r in my:
+            c+=my[r]
+        if presum in my:
+            my[presum]+=1
+        else:
+            my[presum]=1
+     return c
 
-       return cut
-       
+    
