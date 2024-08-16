3bd24e625bd3ff00670ce656c4823dbe07bae330
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-08-16 16:32:58 +0530

    Time: 1361 ms (38.50%) | Memory: 42.6 MB (29.31%) - LeetSync

--- 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
+++ 624-maximum-distance-in-arrays/maximum-distance-in-arrays.py
@@ -1,28 +1,18 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        # min_v=arrays[0][0]
-        # max_v = arrays[0][-1]
-        # maxi=0
-        # for i in range(1,len(arrays)):
-        #     cu_min=arrays[i][0]
-        #     cu_max=arrays[i][-1]
-        #     maxi=max(maxi,abs(min_v - cu_max), abs(max_v - cu_min))
-        # min_v=min(min_v,cu_min)
-        # mac_v=min(max_v, cu_max)
-        # return maxi
-            min_value = arrays[0][0]
-            max_value = arrays[0][-1]
-            max_distance = 0
+        min_v = arrays[0][0]  # Initial minimum value from the first array
+        max_v = arrays[0][-1]  # Initial maximum value from the first array
+        maxi = 0  # To store the maximum distance
+        
+        for i in range(1, len(arrays)):
+            cu_min = arrays[i][0]  # Current array's minimum value
+            cu_max = arrays[i][-1]  # Current array's maximum value
             
-            for i in range(1, len(arrays)):
-                current_min = arrays[i][0]
-                current_max = arrays[i][-1]
-                
-                # Calculate the maximum possible distance with the current array
-                max_distance = max(max_distance, abs(current_max - min_value), abs(max_value - current_min))
-                
-                # Update min and max values for the next iteration
-                min_value = min(min_value, current_min)
-                max_value = max(max_value, current_max)
+            # Calculate the maximum distance considering the current array
+            maxi = max(maxi, abs(min_v - cu_max), abs(max_v - cu_min))
             
-            return max_distance
+            # Update min_v and max_v with the current array's values
+            min_v = min(min_v, cu_min)
+            max_v = max(max_v, cu_max)
+        
+        return maxi
