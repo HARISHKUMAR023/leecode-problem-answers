72145c632e4d627422510a5edd022d251c8a94f3
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-08-18 13:53:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (46.68%) - LeetSync

---
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    int findLUSlength(std::string a, std::string b) {
+        if (a == b) {
+            return -1;
+        } else {
+            return std::max(a.size(), b.size());
+        }
+    }
+};
