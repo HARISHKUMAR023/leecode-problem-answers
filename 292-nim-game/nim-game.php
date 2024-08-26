439fa980e0225b23617bc97bb323a1e3d6cd413b
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-08-26 14:48:26 +0530

    Time: 4 ms (86.96%) | Memory: 19.9 MB (43.48%) - LeetSync

---
+++ 292-nim-game/nim-game.php
@@ -0,0 +1,15 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function canWinNim($n) {
+          if($n%4 == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
