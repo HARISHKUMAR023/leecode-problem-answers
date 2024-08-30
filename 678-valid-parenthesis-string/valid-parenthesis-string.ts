f20b0a41109965dacd0a3eae4b6f51e6d3f00628
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-08-30 07:43:49 +0530

    Time: 41 ms (100.00%) | Memory: 50.2 MB (92.42%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.ts
@@ -0,0 +1,23 @@
+function checkValidString(s: string): boolean {
+    let min = 0;
+        let max =0;
+        for (let i =0; i< s.length;i++){
+            if(s[i] == '('){
+                min+=1;
+                max+=1;
+            }else if (s[i]== ')'){
+                min-=1;
+                max-=1;
+            }else{
+                min-=1;
+                max+=1;
+            }
+            if(min < 0){
+                min = 0 ; 
+            }
+            if(max < 0 ){
+                return false;
+            }
+        }
+        return min == 0 ;
+};
