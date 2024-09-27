9e04fb0778c8455866d30ac44103774c9c0ea739
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2024-09-27 18:59:12 +0530

    Time: 631 ms (16.21%) | Memory: 18.5 MB (90.50%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,11 +1,12 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         my={}
+        a=int(len(candyType) / 2)
         for i in candyType:
             my[i]=my.get(i,0)+1
-        if int(len(candyType) / 2) == len(my):
+        if a == len(my):
             return len(my)
-        elif int(len(candyType) / 2) < len(my):
+        elif a < len(my):
             return int(len(candyType) / 2)
         else:
             return len(my)
